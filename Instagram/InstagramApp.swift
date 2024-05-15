155037297c2cca0aae5e59d4c59df170eab8372c
denysooz <den.darevskiy@mail.ru>
2024-05-15 13:21:16 +0300

    Initial Commit

---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Denis Dareuskiy on 15.05.24.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
