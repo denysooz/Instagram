28f2b5d997c313783bf439d43b040d4c29670ea0
denysooz <den.darevskiy@mail.ru>
2024-06-02 14:12:08 +0300

    Created CicrularProfileImageView and editing photo fetched with feend and search views

---
+++ Instagram/Core/Components/CircularProfileImageView.swift
@@ -0,0 +1,18 @@
+//
+//  CircularProfileImageView.swift
+//  Instagram
+//
+//  Created by Denis Dareuskiy on 30.05.24.
+//
+
+import SwiftUI
+
+struct CircularProfileImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircularProfileImageView()
+}
